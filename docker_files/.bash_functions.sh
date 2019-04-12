c91c088fed556871bfd8ebfe15104519f8decbaf
Michael Durrant <m2@snap2web.com>
2019-04-12 19:44:43 -0400

    more dot files - bash_functions and git completion

---
+++ docker_files/.bash_functions.sh
@@ -0,0 +1,38 @@
+cdc() {
+  for fn in "$@"; do
+    for fn do
+      if [[ "${fn##*/}" == .* ]]
+      then
+        source-highlight --src-lang=sh --out-format=esc -i "$fn"
+      else
+        source-highlight               --out-format=esc -i "$fn"
+      fi 2> /dev/null  ||  /bin/cat "$fn"
+    done
+  done; }; alias cat='cdc' # Keep cat alias here by (cdc) function definition
+md () {
+  [ $# = 1 ] && mkdir -p "$@" && cd "$@" || echo "Error - no directory passed!"
+  }
+bup () {
+  if [[ ! -f ~/$1 ]]
+    then
+      echo "Error - file does not exist"
+      return
+  fi
+  [ $# = 1 ] && {
+    cp -v ~/$1 ~/Dropnot/setups
+    #install -d -v ~/$1 ~/Dropnot/setups/$1   # attempt to avoid no dir issue
+    cd ~/Dropnot/setups
+    git fetch
+    git merge origin/master
+    git add $1
+    git commit -m"$1 update"
+    git push origin master
+    cp -v $1 ~
+    cd -; } || echo "Error - no filename passed!"
+  }
+git_branch () {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+  }
+newalias() {
+  echo "alias ${1}" >> $HOME/.bash_aliases; source ~/.bash_aliases;
+  }
