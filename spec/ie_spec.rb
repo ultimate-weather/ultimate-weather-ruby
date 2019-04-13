9e53756284f33936887301341144dc99e529a4ee
Michael Durrant <m2@snap2web.com>
2019-04-13 19:21:25 -0400

    fix

--- spec/ie_spec.rb
+++ spec/ie_spec.rb
@@ -1,41 +1,20 @@
 describe 'IE browser loads correctly', :type => :feature do
 	before :each do |scenario|
-		require 'selenium/webdriver'
-
-		ENV["BS_USERNAME"]='michaeldurrant3'
-		ENV["BS_AUTHKEY"]='xpVH9NPWHJQLZdR1mbgq'
-
-		url = "http://#{ENV['BS_USERNAME']}:#{ENV['BS_AUTHKEY']}@hub-cloud.browserstack.com/wd/hub"
-
-		capabilities = Selenium::WebDriver::Remote::Capabilities.new
-
-		capabilities['project'] = ENV['BS_AUTOMATE_PROJECT'] if ENV['BS_AUTOMATE_PROJECT']
-		capabilities['build'] = ENV['BS_AUTOMATE_BUILD'] if ENV['BS_AUTOMATE_BUILD']
-
-		capabilities['platform'] = ENV['SELENIUM_PLATFORM'] || 'windows'
-		capabilities['browser'] = ENV['SELENIUM_BROWSER'] || 'IE'
-		capabilities['browser_version'] = ENV['SELENIUM_VERSION'] if ENV['SELENIUM_VERSION']
-
-		browser = Selenium::WebDriver.for(:remote, :url => url, :desired_capabilities => capabilities)
-
-		@browser = browser
-
-    Capybara.current_driver = 'IE'
+    Capybara.app_host = 'http://www.bing.com'
+    Capybara.current_driver = :browserstack
 	end
-
-  after :each do
-    @browser.quit
-  end
 
   it 'IE browser has the expected search input field' do
     visit( @root )
-    search_button_on_page=find(:css, "input[aria-label='Search']")
+    search_button_on_page=find(:css, "input[name='q']")
     expect(search_button_on_page).to be
   end
-  it 'IE browser shows results' do
+  it 'IE browser shows results', :happy do
     visit( @root )
-    find(:css, "input[aria-label='Search']").set("123")
-    find(:css, "input[aria-label='Search']").send_keys :enter
-    expect(page).to have_content 'results'
+    find(:css, "input[name='q']").send_keys "123"
+sleep 2 # or intermittent failures
+    find(:css, "input[name='q']").send_keys :enter
+sleep 3
+    expect(page).to have_content 'result'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,58 @@ Capybara.register_driver :firefox do |app|
   Capybara::Selenium::Driver.new(app, browser: :firefox)
 end
 
+CONFIG = Hash.new
+CONFIG['user'] = 'michaeldurrant3'
+CONFIG['key'] = 'xpVH9NPWHJQLZdR1mbgq'
+
+    require 'selenium/webdriver'
+    require 'browserstack/local'
+    require 'yaml'
+
+    ENV["BS_USERNAME"]='michaeldurrant3'
+    ENV["BS_AUTHKEY"]='xpVH9NPWHJQLZdR1mbgq'
+
+    url = "http://#{ENV['BS_USERNAME']}:#{ENV['BS_AUTHKEY']}@hub-cloud.browserstack.com/wd/hub"
+
+    capabilities = Selenium::WebDriver::Remote::Capabilities.new
+
+    capabilities['project'] = ENV['BS_AUTOMATE_PROJECT'] if ENV['BS_AUTOMATE_PROJECT']
+    capabilities['build'] = ENV['BS_AUTOMATE_BUILD'] if ENV['BS_AUTOMATE_BUILD']
+
+    capabilities['os'] = ENV['SELENIUM_OS'] || 'Windows'
+    capabilities['os_version'] = ENV['SELENIUM_OS_VERSION'] || '10'
+
+    capabilities['platform'] = ENV['SELENIUM_PLATFORM'] || 'windows'
+    capabilities['browser'] = ENV['SELENIUM_BROWSER'] || 'IE'
+    capabilities['browser_version'] = ENV['SELENIUM_VERSION'] if ENV['SELENIUM_VERSION']
+    capabilities['browserstack.local'] = "true"
+
+    @caps = capabilities
+
+    browser = Selenium::WebDriver.for(:remote, :url => url, :desired_capabilities => capabilities)
+
+    @browser = browser
+
+
+
+Capybara.register_driver :browserstack do |app|
+#  @caps = CONFIG['common_caps'].merge(CONFIG['browser_caps'][TASK_ID])
+
+  # Code to start browserstack local before start of test
+#  if @caps['browserstack.local'] && @caps['browserstack.local'].to_s == 'true';
+#    @bs_local = BrowserStack::Local.new
+#    bs_local_args = {"key" => "#{CONFIG['key']}"}
+#    @bs_local.start(bs_local_args)
+#  end
+
+  Capybara::Selenium::Driver.new(app,
+    :browser => :remote,
+    :url => "http://michaeldurrant3:xpVH9NPWHJQLZdR1mbgq@hub-cloud.browserstack.com/wd/hub",
+    :desired_capabilities => @caps
+  )
+end
+
+Capybara.default_driver = :browserstack
 Capybara.default_max_wait_time = 2
 
 RSpec.configure do |config|
