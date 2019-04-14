b375a59142a9ebc22b225daeab83a61d46594ab4
Michael Durrant <m2@snap2web.com>
2019-04-14 06:58:27 -0400

    fix

--- spec/ie_spec.rb
+++ spec/ie_spec.rb
@@ -1,10 +1,8 @@
 describe 'IE browser loads correctly', :type => :feature do
-	before :each do |scenario|
-    Capybara.current_driver = :browserstack
-	end
 
-  it 'IE browser has the expected search input field', :ie do
+  it 'IE browser has the expected search input field on bing.com', :ie do
     if ENV["TEST_IE"] == 'true'
+      Capybara.current_driver = :browserstack
       Capybara.app_host = 'http://www.bing.com'
       visit( @root )
       search_button_on_page=find(:css, "input[name='q']")
@@ -14,8 +12,9 @@ describe 'IE browser loads correctly', :type => :feature do
     end
   end
 
-  it "IE for W3C.org", :ie do
+  it "IE for w3.org", :ie do
     if ENV["TEST_IE"] == 'true'
+      Capybara.current_driver = :browserstack
       Capybara.app_host = 'http://www.w3.org'
       visit @root
       expect(page).to have_content "The World Wide Web Consortium"
