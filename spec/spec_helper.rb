dec28bd2b25d850f644121674b1be22e20b58092
Michael Durrant <m2@snap2web.com>
2019-04-14 07:05:21 -0400

    fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,48 +17,52 @@ Capybara.register_driver :firefox do |app|
   Capybara::Selenium::Driver.new(app, browser: :firefox)
 end
 
-CONFIG = Hash.new
-CONFIG['user'] = 'michaeldurrant3'
-CONFIG['key'] = 'xpVH9NPWHJQLZdR1mbgq'
+if ENV["TEST_IE"]
 
-require 'selenium/webdriver'
-require 'browserstack/local'
-require 'yaml'
+	CONFIG = Hash.new
+	CONFIG['user'] = 'michaeldurrant3'
+	CONFIG['key'] = 'xpVH9NPWHJQLZdR1mbgq'
 
-ENV["BS_USERNAME"]=CONFIG['user']
-ENV["BS_AUTHKEY"]=CONFIG['key']
-url = "http://#{ENV['BS_USERNAME']}:#{ENV['BS_AUTHKEY']}@hub-cloud.browserstack.com/wd/hub"
-capabilities = Selenium::WebDriver::Remote::Capabilities.new
-capabilities['project'] = ENV['BS_AUTOMATE_PROJECT'] if ENV['BS_AUTOMATE_PROJECT']
-capabilities['build'] = ENV['BS_AUTOMATE_BUILD'] if ENV['BS_AUTOMATE_BUILD']
-capabilities['os'] = ENV['SELENIUM_OS'] || 'Windows'
-capabilities['os_version'] = ENV['SELENIUM_OS_VERSION'] || '10'
-capabilities['browser'] = ENV['SELENIUM_BROWSER'] || 'IE'
-capabilities['browser_version'] = ENV['SELENIUM_VERSION'] if ENV['SELENIUM_VERSION']
-capabilities['browserstack.local'] = "true"
-@caps = capabilities
-browser = Selenium::WebDriver.for(:remote, :url => url, :desired_capabilities => capabilities)
-@browser = browser
+	require 'selenium/webdriver'
+	require 'browserstack/local'
+	require 'yaml'
 
-Capybara.register_driver :browserstack do |app|
+	ENV["BS_USERNAME"]=CONFIG['user']
+	ENV["BS_AUTHKEY"]=CONFIG['key']
+	url = "http://#{ENV['BS_USERNAME']}:#{ENV['BS_AUTHKEY']}@hub-cloud.browserstack.com/wd/hub"
+	capabilities = Selenium::WebDriver::Remote::Capabilities.new
+	capabilities['project'] = ENV['BS_AUTOMATE_PROJECT'] if ENV['BS_AUTOMATE_PROJECT']
+	capabilities['build'] = ENV['BS_AUTOMATE_BUILD'] if ENV['BS_AUTOMATE_BUILD']
+	capabilities['os'] = ENV['SELENIUM_OS'] || 'Windows'
+	capabilities['os_version'] = ENV['SELENIUM_OS_VERSION'] || '10'
+	capabilities['browser'] = ENV['SELENIUM_BROWSER'] || 'IE'
+	capabilities['browser_version'] = ENV['SELENIUM_VERSION'] if ENV['SELENIUM_VERSION']
+	capabilities['browserstack.local'] = "true"
+	@caps = capabilities
+	browser = Selenium::WebDriver.for(:remote, :url => url, :desired_capabilities => capabilities)
+	@browser = browser
 
-# Code to start browserstack local before start of test
-#  @caps = CONFIG['common_caps'].merge(CONFIG['browser_caps'][TASK_ID])
-#  if @caps['browserstack.local'] && @caps['browserstack.local'].to_s == 'true';
-#    @bs_local = BrowserStack::Local.new
-#    bs_local_args = {"key" => "#{CONFIG['key']}"}
-#    @bs_local.start(bs_local_args)
-#  end
+	Capybara.register_driver :browserstack do |app|
+
+  # Code to start browserstack local before start of test
+  #  @caps = CONFIG['common_caps'].merge(CONFIG['browser_caps'][TASK_ID])
+  #  if @caps['browserstack.local'] && @caps['browserstack.local'].to_s == 'true';
+  #    @bs_local = BrowserStack::Local.new
+  #    bs_local_args = {"key" => "#{CONFIG['key']}"}
+  #    @bs_local.start(bs_local_args)
+  #  end
 
   Capybara::Selenium::Driver.new(app,
     :browser => :remote,
     :url => "http://michaeldurrant3:xpVH9NPWHJQLZdR1mbgq@hub-cloud.browserstack.com/wd/hub",
     :desired_capabilities => @caps
   )
+
+  end
 end
 
-Capybara.default_driver = :browserstack
 Capybara.default_max_wait_time = 2
+Capybara.default_driver = :chrome
 
 RSpec.configure do |config|
 	config.after(:each) do |example|
