5da480933c972b11728364845aff9ac9b462fbe5
Michael Durrant <m2@snap2web.com>
2019-04-14 20:45:42 -0400

    fix

--- spec/chrome_foo_spec.rb
+++ spec/chrome_foo_spec.rb
@@ -4,7 +4,7 @@ describe 'Chrome browser loads correctly', type: :feature do
   end
   it 'Chrome browser has the expected search input field' do
     visit(@root)
-    search_button_on_page=find(:css, "input[aria-label='Search']")
+    search_button_on_page = find(:css, "input[aria-label='Search']")
     expect(search_button_on_page).to be
   end
   it 'Chrome browser shows results', :happy do
