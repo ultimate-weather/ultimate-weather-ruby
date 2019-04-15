e2db65e5d9f286f0f490cea7233f3ed4b70c35ec
Michael Durrant <m2@snap2web.com>
2019-04-15 10:13:16 -0400

    haml and erb example routes and pages

--- sinatra.rb
+++ sinatra.rb
@@ -17,9 +17,27 @@ get '/zip/46420/current_temp' do
   '67'
 end
 
+get '/haml' do
+  haml :hamilee
+end
+
+get '/haml_with_var' do
+  @day='Monday'
+  @time=Time.now
+  haml :hamilee
+end
+
+get '/erby' do
+  erb :erby
+end
+
 get '/' do
   headers 'Expires' => '0'
   'Welcome to Ultimate Weather'
   "Your params zip is #{params[:zip]}"
+end
+
+not_found do
+  "Something went wrong with the format of your request - and the route wasn't found from that URL"
 end
 
