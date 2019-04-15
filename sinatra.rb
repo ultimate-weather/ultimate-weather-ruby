1028347ef13f1e5cbb32f793d401077f2917d591
Michael Durrant <m2@snap2web.com>
2019-04-15 09:38:12 -0400

    More sinatra route examples

--- sinatra.rb
+++ sinatra.rb
@@ -1,7 +1,12 @@
 require 'sinatra'
 
-get '/' do
-  'Welcome to Ultimate Weather'
+get '/now' do
+  headers "Expires" => "0"
+  'Ultimate Weather - the always reloaded page' + Time.now.to_s
+end
+
+get '/slashed/?' do
+  'Ultimate Weather  - a trailing slash in the URL (or none) will both work here!' + Time.now.to_s
 end
 
 get '/place/indy/current_temp' do
@@ -11,3 +16,10 @@ end
 get '/zip/46420/current_temp' do
   '67'
 end
+
+get '/' do
+  headers 'Expires' => '0'
+  'Welcome to Ultimate Weather'
+  "Your params zip is #{params[:zip]}"
+end
+
